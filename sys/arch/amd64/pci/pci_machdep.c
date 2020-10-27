19a2bd1c908a1f951d35616f42e510d1ea519603
jordan <jordan@openbsd.org>
2020-10-27 02:39:07 +0000

    Adding IOMMU support for AMD Vi and Intel VTD (disabled) This creates separate domains for each PCI device and can provide protection against invalid memory access.  Needed for Passthrough PCI from vmd. ok deraadt@, kettenis@ : ----------------------------------------------------------------------

--- sys/arch/amd64/pci/pci_machdep.c
+++ sys/arch/amd64/pci/pci_machdep.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: pci_machdep.c,v 1.75 2020/06/17 06:14:52 dlg Exp $	*/
+/*	$OpenBSD: pci_machdep.c,v 1.76 2020/10/27 02:39:07 jordan Exp $	*/
 /*	$NetBSD: pci_machdep.c,v 1.3 2003/05/07 21:33:58 fvdl Exp $	*/
 
 /*-
@@ -90,6 +90,8 @@
 #endif
 
 #include "acpi.h"
+
+#include "acpidmar.h"
 #if NACPIDMAR > 0
 #include <dev/acpi/acpidmar.h>
 #endif
@@ -802,14 +804,14 @@ pci_init_extents(void)
 	}
 }
 
-#if NACPIDMAR > 0
 int
 pci_probe_device_hook(pci_chipset_tag_t pc, struct pci_attach_args *pa)
 {
+#if NACPIDMAR > 0
 	acpidmar_pci_hook(pc, pa);
+#endif
 	return 0;
 }
-#endif
 
 #if NACPI > 0
 void acpi_pci_match(struct device *, struct pci_attach_args *);
